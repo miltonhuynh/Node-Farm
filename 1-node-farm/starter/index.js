f3e26e720dd36eff385e4f0b75a23f83ca3cec59
miltonhuynh <miltonhuynh@gmail.com>
2022-05-13 14:03:01 -0700

    Checkpoint: finished section 2-10

--- 1-node-farm/starter/index.js
+++ 1-node-farm/starter/index.js
@@ -9,9 +9,16 @@ fs.writeFileSync('./txt/output.txt', textOut);
 console.log('File has been written');
 
 // Non-blocking, asynchronous way
- fs.readFile('./txt/start.txt', 'utf-8', (err, data1) => {
+ fs.readFile('./teext/start.txt', 'utf-8', (err, data1) => {
+    if (err) return console.log("error");
    fs.readFile(`./txt/${data1}.txt`, 'utf-8', (err, data2) => {
       console.log(data2);
+      fs.readFile(`./txt/append.txt`, 'utf-8', (err, data3) => {
+         console.log(data3);
+         fs.writeFile('./txt/final.txt', `${data2}\n\n${data3}`, 'utf-8', err => {
+            console.log('Your file has been written');
+         });
+      });
    });
  });
 
